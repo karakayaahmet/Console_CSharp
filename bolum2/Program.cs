069da6255b746d529e583e8522900fe957dca046
Ahmet Karakaya <karakayahmet00@gmail.com>
2023-01-26 22:07:36 +0300

    bolum2 tamamlandı.

--- bolum2/Program.cs
+++ bolum2/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string name = "fırat";
+string surname = "dicle";
+int age = 100;
+
+string cumle1 = "Adı " + name + " soyadı " + surname + " olan yapının yaşı " + age + " dır.";
+Console.WriteLine(cumle1);
+
+string cumle2 = string.Format("Adı {0} soyadı {1} olan yapının yaşı {2} dır", name, surname, age);
+Console.WriteLine(cumle2);
+
+string cumle3 = $"Adı {name} soyadı {surname} olan yapının yaşı {age} dır.";
+Console.WriteLine(cumle3);
