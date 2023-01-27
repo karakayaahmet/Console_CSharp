f3685b61ea489deff38e438c0d8548c5242582fe
Ahmet Karakaya <karakayahmet00@gmail.com>
2023-01-27 12:05:32 +0300

    bölüm 6 tamamlandı.

--- bolum6/Program.cs
+++ bolum6/Program.cs
@@ -1,2 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+DateTime simdi = DateTime.Now;
+Console.WriteLine(simdi);
+Console.WriteLine(simdi.Year);
+Console.WriteLine(simdi.Month);
+Console.WriteLine(simdi.Day);
+Console.WriteLine(simdi.DayOfWeek);
+Console.WriteLine(simdi.DayOfYear);
+
+Console.WriteLine(simdi.Hour);
+Console.WriteLine(simdi.Minute);
+Console.WriteLine(simdi.Second);
+
+DateTime dt = new DateTime(2018, 6, 30, 14, 43, 43);
+Console.WriteLine(dt.AddDays(7));
+Console.WriteLine(dt.AddHours(6));
+Console.WriteLine(dt.AddYears(3));
+Console.WriteLine(dt.AddMonths(6));
+
+var fark = simdi - dt;
+
+Console.WriteLine(fark.TotalDays);
+Console.WriteLine(fark.TotalHours);
+Console.WriteLine(fark.TotalMinutes);
+Console.WriteLine(fark.TotalSeconds);
+
+Console.WriteLine(simdi.ToString("d"));
+Console.WriteLine(simdi.ToString("D"));
+
+Console.WriteLine(simdi.ToString("F"));
+Console.WriteLine(simdi.ToString("M"));
+
+Console.WriteLine(simdi.ToString("t"));
+Console.WriteLine(simdi.ToString("T"));
+
+Console.WriteLine(simdi.ToString("Y"));
+Console.WriteLine(simdi.ToShortDateString());
+
+Console.WriteLine(simdi.ToString("hh:mm:ss"));
+Console.WriteLine(simdi.ToString("ddd MMM %d, yyyy"));
+
