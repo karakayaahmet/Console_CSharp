a531ac9f246b78db7bcf13f936f08a9c0b4e75dd
Ahmet Karakaya <karakayahmet00@gmail.com>
2023-01-28 12:49:10 +0300

    bölüm 9 tamamlandı.

--- bolum9/Program.cs
+++ bolum9/Program.cs
@@ -1,8 +1,35 @@
 ﻿namespace bolum9;
+
+class Person{
+    public string Name { get; set; }
+    public int Year { get; set; }
+
+    public string Intro(){
+        return $"Kişi Adı : {this.Name}, yaşı : {2023-this.Year}";
+    }
+
+    public int CalculateAge(){
+        return DateTime.Now.Year - this.Year;
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var p1 = new Person{Name = "Mehmet", Year = 1985};
+        var p2 = new Person{Name = "Akif", Year = 1993};
+        var p3 = new Person{Name = "Ersoy", Year = 2000};
+
+        var str1 = p1.Intro();
+        var str2 = p2.Intro();
+        var str3 = p3.Intro();
+
+        Console.WriteLine(str1);
+        Console.WriteLine(str2);
+        Console.WriteLine(str3);
+        Console.WriteLine(p1.CalculateAge());
+        Console.WriteLine(p2.CalculateAge());
+        Console.WriteLine(p3.CalculateAge());
     }
 }
