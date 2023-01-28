cea93f2a131f42d8ac81cddb0d8a229462ee97d6
Ahmet Karakaya <karakayahmet00@gmail.com>
2023-01-28 11:59:00 +0300

    bölüm 8 yeniden oluşturuldu.

--- bolum8/Program.cs
+++ bolum8/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace bolum8;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
