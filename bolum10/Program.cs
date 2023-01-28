a168b348d65d0e0a5e38549f8123a5321d6a6032
Ahmet Karakaya <karakayahmet00@gmail.com>
2023-01-28 12:58:45 +0300

    bölüm 10 tamamlandı.

--- bolum10/Program.cs
+++ bolum10/Program.cs
@@ -3,6 +3,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+      var islem = new Islem();
+      Console.WriteLine(islem.Toplam(12,2,3,5));
+      Console.WriteLine(islem.Toplam(342,213));
+      Console.WriteLine(islem.carp(12,2,3));
+    }
+}
+
+class Islem{
+
+    public int Toplam(params int[] sayilar){
+        var toplam = 0;
+        foreach(var sayi in sayilar){
+            toplam += sayi;
+        }
+
+        return toplam;
+    }
+
+    public int carp(params int[] numbers){
+        var carpma = 1;
+        foreach(var x in numbers){
+            carpma *= x;
+        }
+        return carpma;
     }
 }
