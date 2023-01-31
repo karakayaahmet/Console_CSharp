61c20dfb7df7fbc6c1cc162eeea5165dcc02b54e
Ahmet Karakaya <karakayahmet00@gmail.com>
2023-01-31 17:31:05 +0300

    bölüm 19 tamamlandı.

--- bolum19/Program.cs
+++ bolum19/Program.cs
@@ -1,8 +1,41 @@
 ﻿namespace bolum19;
+using System;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        try{
+            Console.Write("Kullanıcı adı : ");
+            string username = Console.ReadLine();
+
+            Console.Write("Şifre : ");
+            string password = Console.ReadLine();
+
+            Login(username, password);
+
+            Console.WriteLine("Giriş Başarılı ;)");
+        }
+
+        catch(LoginException e){
+            Console.WriteLine(e.Message);
+        }
+        
+    }
+
+    static void Login(string username, string password){
+        if (username.Contains(" ")){
+            throw new LoginException("Kullanıcı adı boşluk içeremez!");
+        }
+
+        if (password.Length<8){
+            throw new LoginException("Şifre minimum 8 karakterden oluşmalıdır !");
+        }
+    }
+}
+
+class LoginException : Exception{
+
+    public LoginException(string message):base(message){
+
     }
 }
