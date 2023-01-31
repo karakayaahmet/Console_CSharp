f8550b6ca8faee59abf2ef05771cd69439a01763
Ahmet Karakaya <karakayahmet00@gmail.com>
2023-01-31 12:54:39 +0300

    bölüm 16 tamamlandı.

--- bolum16/Program.cs
+++ bolum16/Program.cs
@@ -1,8 +1,29 @@
 ﻿namespace bolum16;
+using System.Collections.Generic;
+using System.Linq;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Dictionary<int, string> plakalar = new Dictionary<int, string>();
+
+        plakalar.Add(23, "Elazığ");
+        plakalar.Add(06, "Ankara");
+        plakalar.Add(34, "İstanbul");
+        plakalar.Add(35, "İzmir");
+
+        foreach (var item in plakalar)
+        {
+            Console.WriteLine($"Plaka {item.Key} {item.Value}");
+        }
+
+        for (int i = 0; i < plakalar.Count; i++)
+        {
+            Console.WriteLine($"Plaka {plakalar.Keys.ElementAt(i)} {plakalar.Values.ElementAt(i)}");
+        }
+
+        Console.WriteLine("*****************************");
+
+        Console.WriteLine(plakalar.ContainsKey(34));
     }
 }
