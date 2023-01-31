3cdf6cd9dc8b3b88432b684127862d6ee8b574ed
Ahmet Karakaya <karakayahmet00@gmail.com>
2023-01-31 13:11:12 +0300

    bölüm 17 tamamlandı.

--- bolum17/Program.cs
+++ bolum17/Program.cs
@@ -3,6 +3,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        try
+        {
+            Console.WriteLine("1. Sayı : ");
+            int sayi1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("2. Sayı : ");
+            int sayi2 = int.Parse(Console.ReadLine());
+
+            int sonuc = sayi1 / sayi2;
+
+            Console.WriteLine($"{sayi1} / {sayi2} = {sonuc}");
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Bir hata oluştu.");
+            Console.WriteLine(ex.Message);
+        } 
+
+        finally{
+            Console.WriteLine("Finally bloğu çalıştı.");
+        }      
     }
 }
