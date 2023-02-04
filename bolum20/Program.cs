3ab769e8b9663098c6740aa96570b6ae2c276874
Ahmet Karakaya <karakayahmet00@gmail.com>
2023-02-04 15:27:34 +0300

    veritabanına bağlantı işlemi tamamlandı.

--- bolum20/Program.cs
+++ bolum20/Program.cs
@@ -1,12 +1,35 @@
 ﻿namespace bolum20;
+using System;
+using MySql.Data.MySqlClient;
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        GetMySqlConnection();
     }
 
-    static void GetMySqlConnection(){
+    static void GetMySqlConnection()
+    {
+
+        string connectionString = @"server=localhost; port=3306; database=northwind; user=root; password=mysql123;";
+
+        using (var connection = new MySqlConnection(connectionString)){
+
+            try{
+                connection.Open();
+                Console.WriteLine("Bağlantı Sağlandı.");
+            }
+
+            catch(Exception e){
+                Console.WriteLine(e.Message);
+            }
+
+            finally{
+                connection.Close();
+            }
+            
+        }
 
     }
 }
