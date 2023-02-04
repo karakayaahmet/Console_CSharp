3178f72e4ea1a78b1c2e38b76d42ba7f8d4c1ef0
Ahmet Karakaya <karakayahmet00@gmail.com>
2023-02-04 19:00:32 +0300

    bölüm 21 tamamlandı.

--- bolum21/Program.cs
+++ bolum21/Program.cs
@@ -1,8 +1,43 @@
 ﻿namespace bolum21;
+using MySql.Data.MySqlClient;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        GetAllProducts();
+    }
+
+    static void GetAllProducts(){
+        using(var connection = getMysqlConnection()){
+            try{
+                connection.Open();
+
+                string sql = "select * from products";
+
+                MySqlCommand command = new MySqlCommand(sql, connection);
+
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while(reader.Read()){
+                    Console.WriteLine($"name : {reader[3]} price : {reader[6]}");
+                }
+
+                reader.Close();
+
+            }
+
+            catch(Exception e){
+                Console.WriteLine(e.Message);
+            }
+
+            finally{
+                connection.Close();
+            }
+        }
+    }
+
+    static MySqlConnection getMysqlConnection(){
+        string connection = @"server=localhost; port=3306; database=northwind; user=root; password=mysql123;";
+        return new MySqlConnection(connection);
     }
 }
